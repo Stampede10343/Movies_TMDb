6bece52b7b142aca627d9bc45051293ed58cece5
Cameron Cordes <cameronc.dev@gmail.com>
2019-01-19 15:49:54 -0500

    Fixed build error

--- Movies/app/src/main/java/com/dev/cameronc/movies/MoviesApp.kt
+++ Movies/app/src/main/java/com/dev/cameronc/movies/MoviesApp.kt
@@ -12,9 +12,12 @@ import com.dev.cameronc.movies.di.prod.DaggerAppComponent
 import com.squareup.leakcanary.LeakCanary
 import net.danlew.android.joda.JodaTimeAndroid
 import timber.log.Timber
+import javax.inject.Inject
 
 
 class MoviesApp : Application() {
+    @Inject
+    lateinit var observableConnectivityManager: ObservableConnectivityManager
     private lateinit var appComponent: AppComponent
 
     override fun onCreate() {
@@ -28,14 +31,36 @@ class MoviesApp : Application() {
         LeakCanary.install(this)
 
         app = this
+        setupComponent()
+        appComponent.inject(this)
 
         val intentFilter = IntentFilter().apply { addAction(ConnectivityManager.CONNECTIVITY_ACTION) }
         registerReceiver(observableConnectivityManager, intentFilter)
     }
 
+    private fun setupComponent() {
+        appComponent = if (isTestComponentEnabled()) {
+            Timber.d("Test component installed")
+            DaggerTestAppComponent.builder()
+                    .app(this)
+                    .appModule(AppModule(this))
+                    .build()
+        }
+        else {
+            Timber.d("Application component installed")
+            DaggerAppComponent.builder()
+                    .app(this)
+                    .appModule(AppModule(this))
+                    .build()
+        }
+    }
+
+    private fun isTestComponentEnabled() = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(COMPONENT, false)
+
     fun getAppComponent(): AppComponent = appComponent
 
     companion object {
+        const val COMPONENT = "component"
         lateinit var app: MoviesApp
         lateinit var activityComponent: ActivityComponent
     }
